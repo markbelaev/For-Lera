7a0d15dc4b0a4f5e273ebb4218df538d43a03a76
Mark Belaev <markbelaevv@gmail.com>
2025-09-09 20:35:09 +0500

    добавил комплиментов

--- inetrnal/handlers/commands.go
+++ inetrnal/handlers/commands.go
@@ -5,3 +5,22 @@ import "gopkg.in/telebot.v4"
 func handleStart(c telebot.Context) error {
 	return c.Send("Привет")
 }
+
+func handleCompliments(c telebot.Context) error {
+	compliments := []string{
+		"Ты самая красивая",
+		"Ты самая умная",
+		"Ты самая сильная",
+		"У тебя самые красивые глаза",
+		"У тебя самые сочные губы",
+		"Ты самая сексуальная",
+		"Ты самая желанная",
+		"Я теку от тебя",
+		"Ты лучшая мама",
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	randomCompliments := compliments[rand.Intn(len(compliments))]
+
+	return c.Send(randomCompliments)
+}
