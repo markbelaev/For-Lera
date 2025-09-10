e54e112fad562f193255aa18ab641f86031f0910
Mark Belaev <markbelaevv@gmail.com>
2025-09-10 19:55:29 +0500

    Update

--- inetrnal/handlers/commands.go
+++ inetrnal/handlers/commands.go
@@ -14,7 +14,7 @@ func handleStart(c telebot.Context) error {
 
 	// Логируем успешную обработку
 
-	slog.Info("Успешня обработка комманды /start!")
+	slog.Info("Успешная обработка комманды /start!")
 
 	// Возвращаем ответ
 
@@ -25,7 +25,7 @@ func handleStart(c telebot.Context) error {
 // Обработчик команды /compliments
 
 func handleCompliments(c telebot.Context) error {
-	slog.Info("Успешня обработка комманды /compliments!")
+	slog.Info("Успешная обработка комманды /compliments!")
 
 	//Достаем из data список нужных сообщений и присылаем рандомное
 
@@ -38,7 +38,7 @@ func handleCompliments(c telebot.Context) error {
 // Обработчик команды /motivations
 
 func handleMotivation(c telebot.Context) error {
-	slog.Info("Успешня обработка комманды /motivations!")
+	slog.Info("Успешная обработка комманды /motivations!")
 
 	randomIndex := rand.Intn(len(data.Motivations))
 	randomMotivation := data.Motivations[randomIndex]
@@ -49,7 +49,7 @@ func handleMotivation(c telebot.Context) error {
 // Обработчик команды /dreams
 
 func handleDreams(c telebot.Context) error {
-	slog.Info("Успешня обработка комманды /dreams!")
+	slog.Info("Успешная обработка комманды /dreams!")
 
 	randomIndex := rand.Intn(len(data.Dreams))
 	randomDream := data.Dreams[randomIndex]
@@ -60,7 +60,7 @@ func handleDreams(c telebot.Context) error {
 // Обработчик для проверки чувств
 
 func handleLoveQuestion(c telebot.Context) error {
-	slog.Info("Успешня обработка кнопки test love!")
+	slog.Info("Успешная обработка кнопки test love!")
 
 	return c.Send("Ты меня любишь, пупс? 🤔", TestLoveKeyboard())
 }
@@ -84,7 +84,7 @@ func handleNo(c telebot.Context) error {
 // Обработчик на копку "сколько закомы"
 
 func handleTimeLove(c telebot.Context) error {
-	slog.Info("Успешня обработка кнопки time love!")
+	slog.Info("Успешная обработка кнопки time love!")
 
 	return c.Send("Если честно сам не знаю((( \n\n Потом посчитаю", MenuKeyboard())
 }
